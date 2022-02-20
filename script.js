3dd0b452689b182d83445108b8a843b79c854a97
Phil <happy.melodist@gmail.com>
2022-02-20 00:59:12 -0600

    create all of the state objects within the animationStates array

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ playerImage.src = "shadow_dog.png";
 
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+// let frameX = 0;
+// let frameY = 0;
+
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [];
@@ -24,23 +25,65 @@ const animationStates = [
     frames: 7,
   },
   {
-    name: "jump",
+    name: "fall",
     frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
   },
 ];
 
-animationStates.forEach((state) => {});
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  let position = Math.floor(gameFrame / staggerFrames) % 6;
-  frameX = spriteWidth * position;
+  let position =
+    Math.floor(gameFrame / staggerFrames) % spriteAnimations["idle"].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations["idle"].loc[position].y;
 
   ctx.drawImage(
     playerImage,
     frameX,
-    frameY * spriteHeight,
+    frameY,
     spriteWidth,
     spriteHeight,
     0,
