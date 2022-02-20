6d32575f293999e7eea04780ce3d7f5b62b29ce7
Phil <happy.melodist@gmail.com>
2022-02-19 22:38:45 -0600

    complete canvas setup and add sprite sheet to project

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+// console.log(ctx);
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
